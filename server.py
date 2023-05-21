739c4a66b59de3b8e6eb81b2759054211eacb4f3
Abhijith <ajk0x00@gmail.com>
2023-05-21 22:54:58 +0530

    server improvised

--- server.py
+++ server.py
@@ -7,6 +7,7 @@ from networking import Network
 class VideoServer:
 	def __init__(self) -> None:
 		self.server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+		self.camera = VideoStream(-1, resolution=(280,280), framerate=24).start()
 		host_ip = Network().get_host_ip()
 		port = 9999
 		self.socket_address = (host_ip,port)
@@ -21,15 +22,15 @@ class VideoServer:
 			try:
 				print('GOT CONNECTION FROM:',addr)
 				if client_socket:
-					cf = VideoStream(-1, resolution=(280,280), framerate=24).start()
-					while(cf.stream.isOpened()):
-						frame = cf.read()
+					while(self.camera.stream.isOpened()):
+						frame = self.camera.read()
 						frame = imutils.resize(frame,width=280)
 						a = pickle.dumps(frame)
 						message = struct.pack("Q",len(a))+a
 						client_socket.sendall(message)
 						if cv2.waitKey(1) == '13':
 							client_socket.close()
-			except KeyboardInterrupt:
+			except:
 				print("close")
 				client_socket.close()
+VideoServer().start_server()
